fa5297256d19f6abdc5471f4f106d1e5276cd9fb
onezerokang <onezerokang@gmail.com>
2023-07-15 13:15:45 +0900

    blue/green 무중단 배포 스크립트 추가

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,23 @@
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "[$NOW_TIME] No WAS is connected to nginx"
+fi
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+  echo "Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+
+echo "Now new WAS runs at ${TARGET_PORT}."
+
+exit 0
