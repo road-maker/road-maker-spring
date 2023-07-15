63776e61c69f9414200d4411a107ac359dd8761c
onezerokang <onezerokang@gmail.com>
2023-07-15 12:00:24 +0900

    first commit

---
+++ src/main/java/com/roadmaker/RoadmakerApplication.java
@@ -0,0 +1,13 @@
+package com.roadmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadmaker/RoadmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
