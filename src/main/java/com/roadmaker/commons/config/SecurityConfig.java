c0a3dabf500588a8c7ad10f90571315a89749559
onezerokang <onezerokang@gmail.com>
2023-07-16 13:59:41 +0900

    [#2] Feat: Spring Security 초기 설정

---
+++ src/main/java/com/roadmaker/commons/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.roadmaker.commons.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic((httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()))
+                .csrf((csrf) -> csrf.disable())
+                .formLogin((httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable()));
+
+        return httpSecurity
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/users/**").permitAll()
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers("/health").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/roadmaker/member/service/MemberServiceImpl.java
@@ -0,0 +1,8 @@
+package com.roadmaker.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+}
