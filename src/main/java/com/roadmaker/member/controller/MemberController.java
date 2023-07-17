986addccff9b90118a1f956da6011665c1795757
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2023-07-17 16:19:24 +0900

    login api

--- src/main/java/com/roadmaker/member/controller/MemberController.java
+++ src/main/java/com/roadmaker/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.roadmaker.member.controller;
 
 import com.roadmaker.member.domain.entity.Member;
+import com.roadmaker.member.dto.LoginRequest;
 import com.roadmaker.member.dto.SignupRequest;
+import com.roadmaker.member.dto.TokenInfo;
 import com.roadmaker.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
@@ -35,5 +39,17 @@ public class MemberController {
         // 저장 후 201 status 응답
         memberService.signUp(member);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public TokenInfo login(@RequestBody LoginRequest loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        TokenInfo tokenInfo = memberService.login(email, password);
+        return tokenInfo;
+    }
+    @PostMapping("/test")
+    public String test() {
+        return "success";
     }
 }
--- src/main/java/com/roadmaker/member/service/MemberService.java
+++ src/main/java/com/roadmaker/member/service/MemberService.java
@@ -1,12 +1,18 @@
 package com.roadmaker.member.service;
 
 import com.roadmaker.member.domain.entity.Member;
+import com.roadmaker.member.dto.TokenInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public interface MemberService {
     // 회원가입
     public void signUp(Member member);
 
     // 로그인
+    public TokenInfo login(String email, String password);
 
     // 중복 가입 검사
     public boolean isUserRegistered(String email);
