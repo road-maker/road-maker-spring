7b9cad2cbe1c47a3e1c7ed61632569a68d5591dd
Heeho Lim <heeho1998@gmail.com>
2023-07-22 21:46:47 +0900

    [#18} Feat : convert to json

--- src/main/java/com/roadmaker/gpt/controller/GptController.java
+++ src/main/java/com/roadmaker/gpt/controller/GptController.java
@@ -1,12 +1,12 @@
 package com.roadmaker.gpt.controller;
-
-import com.roadmaker.gpt.dto.ChatMessagePrompt;
-import com.theokanning.openai.completion.CompletionRequest;
+import com.roadmaker.gpt.dto.Sentence;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 public class GptController {
-
     private final String OPENAI_TOKEN;
     private static final Duration DURATION = Duration.ofSeconds(60);
 
@@ -22,27 +21,13 @@ public class GptController {
         this.OPENAI_TOKEN = apiKey;
     }
 
-    @GetMapping("/getChat")
-    public String getPrompt(@RequestParam String prompt) {
-        OpenAiService service = new OpenAiService(OPENAI_TOKEN, DURATION);
-
-        String message = String.format("%s",prompt);
-
-        CompletionRequest completionRequest = CompletionRequest.builder().prompt(message).model("text-davinci-003")
-                .echo(true).build();
-//                .maxTokens(10000000)
-
-        return service.createCompletion(completionRequest).getChoices().get(0).getText();
-    }
-
     @PostMapping("/chat")
-    public String getChatMessages(@RequestParam String prompt) {
+    public List<Sentence> getChatMessages(@RequestParam String prompt) {
 
-//        기존에 받던 Body argument : @RequestBody ChatMessagePrompt prompt
         OpenAiService service = new OpenAiService(OPENAI_TOKEN, DURATION);
         List<ChatMessage> messages = new ArrayList<>();
 
-        String content1 = "you are a senior developer. You must return only message.content in the form \"1.title\n-item1\nitem2\nitem3\nitem4\n2.title\n-item1\nitem2\nitem3\nitem4\n3.title\n-item1\nitem2\nitem3\nitem4\n4.title \n-item1\nitem2\nitem3\nitem4\n\"";
+        String content1 = "you are a senior developer. You must return only message.content in the form \"1.title\n1a.item1\n1b.item2\n1c.item3\n1d.item4\n2.title\n2a.item1\n2b.item2\n2c.item3\n2d.item4\n3.title\n3a.item1\n3b.item2\n3c.item3\n3d.item4\n4.title \n4a.item1\n4b.item2\n4c.item3\n4d.item4\n\"";
         String content2 = String.format("Create a roadmap to %s in four title, each with four courses.",prompt);
 
         ChatMessage message1 = new ChatMessage("system", content1 );
@@ -54,7 +39,33 @@ public class GptController {
         ChatCompletionRequest completionRequest = ChatCompletionRequest.builder().messages(messages)
                 .model("gpt-3.5-turbo").build();
 
-        return service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
-    }
+        String gptCompletion = service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
 
+        List<Sentence> sentences = new ArrayList<>();
+
+        // Parsing gptCompletion string
+        String[] lines = gptCompletion.split("\n");
+        List<Sentence> formattedSentences = new ArrayList<>();
+
+        for (String line : lines) {
+            // Trim any leading or trailing spaces
+            line = line.trim();
+
+            // Check if the line contains a dot (.) character
+            if (line.contains(".")) {
+                // Split the line into ID and content based on the dot
+                String[] parts = line.split("\\.", 2); // Split into two parts at the first dot
+
+                // Extract the ID and content from the parts
+                String id = parts[0].trim();
+                String content = parts[1].trim();
+
+                // Create and add a new Sentence object to the list
+                Sentence sentence = new Sentence(id, content);
+                formattedSentences.add(sentence);
+            }
+        }
+
+        return formattedSentences;
+    }
 }
