658abdd7e5b1d96aae92f00f6ace87d62d4a7f3f
Wonyoung Kang <onezerokang@gmail.com>
2023-07-23 15:39:18 +0900

    [#13] Feat: RoadmapNodeDto.java DTO 구

--- src/main/java/com/roadmaker/roadmap/dto/RoadmapNodeDto.java
+++
@@ -1,9 +0,0 @@
-package com.roadmaker.roadmap.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
-@Builder @Data @AllArgsConstructor
-public class RoadmapNodeDto {
-}
