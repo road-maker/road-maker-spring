0b23024d6adf8092027895b3b2497c0df6b45431
Wonyoung Kang <onezerokang@gmail.com>
2023-07-24 01:53:33 +0900

    [#13] Fix: Viewport Entity에 id 필드 추가

--- src/main/java/com/roadmaker/roadmap/entity/roadmapviewport/RoadmapViewport.java
+++ src/main/java/com/roadmaker/roadmap/entity/roadmapviewport/RoadmapViewport.java
@@ -1,12 +1,17 @@
 package com.roadmaker.roadmap.entity.roadmapviewport;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Entity
 public class RoadmapViewport {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private BigDecimal x;
     private BigDecimal y;
     private BigDecimal zoom;
