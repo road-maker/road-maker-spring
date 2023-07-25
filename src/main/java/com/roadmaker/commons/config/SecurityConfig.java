73a3dba968f94856183aa682f3b9b1770b258b0b
Wonyoung Kang <onezerokang@gmail.com>
2023-07-25 16:50:37 +0900

    [#46] Remove: JwtAuthenticationFilter.java 파일 삭제

--- src/main/java/com/roadmaker/commons/config/SecurityConfig.java
+++ src/main/java/com/roadmaker/commons/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.roadmaker.commons.config;
 
-import com.roadmaker.member.authentication.JwtAuthenticationFilter;
-import com.roadmaker.member.authentication.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
