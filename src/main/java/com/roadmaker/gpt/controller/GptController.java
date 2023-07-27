9d2715ee51f00c36710609af09b41abcf0e72066
Heeho Lim <heeho1998@gmail.com>
2023-07-27 23:55:35 +0900

    [#18] Feat : add detail content

--- src/main/java/com/roadmaker/gpt/controller/GptController.java
+++ src/main/java/com/roadmaker/gpt/controller/GptController.java
@@ -1,6 +1,7 @@
 package com.roadmaker.gpt.controller;
 
 import com.roadmaker.commons.annotation.LoginRequired;
+import com.roadmaker.gpt.dto.GptDetailResponse;
 import com.roadmaker.gpt.dto.GptRoadmapResponse;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
@@ -23,15 +24,6 @@ public class GptController {
         this.OPENAI_TOKEN = apiKey;
     }
 
-//    static boolean isKorean(String prompt) {
-//        // Korean Unicode ranges
-//        int start = 0xAC00; // 가 (Hangul Syllables)
-//        int end = 0xD7AF;   // 힣 (Hangul Syllables)
-//
-//        return prompt.codePoints().anyMatch(c -> c >= start && c <= end);
-//    }
-
-
     @LoginRequired
     @PostMapping("api/chat")
     public List<GptRoadmapResponse> getChatMessages(@RequestParam String prompt) {
@@ -42,13 +34,8 @@ public class GptController {
         String content1;
         String content2;
 
-//        if(isKorean(prompt)) {
         content1 = "너는 한국인 개발자이다. 반드시 message.content를 \"1.title\n1a.item1\n1b.item2\n1c.item3\n1d.item4\n2.title\n2a.item1\n2b.item2\n2c.item3\n2d.item4\n3.title\n3a.item1\n3b.item2\n3c.item3\n3d.item4\n4.title \n4a.item1\n4b.item2\n4c.item3\n4d.item4\n\" 이 형식에 맞춰서 대답해주세요.";
         content2 = String.format("각각 4개의 코스로 구성된 4개의 타이틀에서 %s를 위한 로드맵을 만들어주세요.", prompt);
-//        } else {
-//            content1 = "you are a senior developer. You must return only message.content in the form \"1.title\n1a.item1\n1b.item2\n1c.item3\n1d.item4\n2.title\n2a.item1\n2b.item2\n2c.item3\n2d.item4\n3.title\n3a.item1\n3b.item2\n3c.item3\n3d.item4\n4.title \n4a.item1\n4b.item2\n4c.item3\n4d.item4\n\"";
-//            content2 = String.format("Create a roadmap to %s in four title, each with four courses.",prompt);
-//        }
 
         ChatMessage message1 = new ChatMessage("system", content1 );
         ChatMessage message2 = new ChatMessage("user", content2 );
@@ -74,7 +61,7 @@ public class GptController {
                 // 점을 기준으로 줄을 ID와 콘텐츠로 분할합니다.
                 String[] parts = line.split("\\.", 2); // Split into two parts at the first dot
 
-                // parts에서 ID와 content를 추출합니다.
+                // parts에서 ID와 content를 추출합니다. (trim은 공백 제거)
                 String id = parts[0].trim();
                 String content = parts[1].trim();
 
@@ -85,5 +72,30 @@ public class GptController {
         }
 
         return formattedGptRoadmapResponses;
+    }
+
+    @LoginRequired
+    @PostMapping("api/nodecontent")
+    public GptDetailResponse detailContent(@RequestParam String course){
+        OpenAiService service = new OpenAiService(OPENAI_TOKEN, DURATION);
+        List<ChatMessage> messages = new ArrayList<>();
+
+        String content1;
+        String content2;
+
+        content1 = "너는 한국인 개발자이다. 입력하는 값의 개념을 반드시 한 단락으로 설명해줘.";
+        content2 = String.format(course);
+
+        ChatMessage message1 = new ChatMessage("system", content1 );
+        ChatMessage message2 = new ChatMessage("user", content2 );
+
+        messages.add(message1);
+        messages.add(message2);
+
+        ChatCompletionRequest completionRequest = ChatCompletionRequest.builder().messages(messages)
+                .model("gpt-3.5-turbo").build();
+
+        GptDetailResponse gptDetailResponse = new GptDetailResponse(service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent());
+        return gptDetailResponse;
     }
 }
