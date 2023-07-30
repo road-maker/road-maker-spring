059a3bfb805eac83827a736fd36d27df5520b540
Wonyoung Kang <onezerokang@gmail.com>
2023-07-31 00:14:52 +0900

    [#113] Feat: Roadmap entity에 member 필드 추가

--- src/main/java/com/roadmaker/roadmap/entity/roadmap/Roadmap.java
+++ src/main/java/com/roadmaker/roadmap/entity/roadmap/Roadmap.java
@@ -2,6 +2,7 @@ package com.roadmaker.roadmap.entity.roadmap;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.roadmaker.commons.BaseTimeEntity;
+import com.roadmaker.member.entity.Member;
 import com.roadmaker.roadmap.entity.roadmapedge.RoadmapEdge;
 import com.roadmaker.roadmap.entity.roadmapeditor.RoadmapEditor;
 import com.roadmaker.roadmap.entity.roadmapnode.RoadmapNode;
@@ -26,6 +27,10 @@ public class Roadmap extends BaseTimeEntity {
 
     private String thumbnailUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
     @OneToMany(mappedBy = "roadmap")
     @JsonManagedReference
     private List<RoadmapNode> roadmapNodes;
@@ -45,9 +50,10 @@ public class Roadmap extends BaseTimeEntity {
 
 
     @Builder
-    public Roadmap(String title, String description, String thumbnailUrl) {
+    public Roadmap(String title, String description, String thumbnailUrl, Member member) {
         this.title = title;
         this.description = description;
         this.thumbnailUrl = thumbnailUrl;
+        this.member = member;
     }
 }
