c5265d3a52345c6d83b83b5c41fdd9fb51b3b29f
Wonyoung Kang <onezerokang@gmail.com>
2023-07-31 21:20:21 +0900

    [#130] Feat: RoadmapResponse가 계층 구조를 갖도록 하기

--- src/main/java/com/roadmaker/roadmap/dto/RoadmapResponse.java
+++ src/main/java/com/roadmaker/roadmap/dto/RoadmapResponse.java
@@ -1,9 +1,11 @@
 package com.roadmaker.roadmap.dto;
 
+import com.roadmaker.member.dto.MemberResponse;
 import com.roadmaker.roadmap.entity.inprogressnode.InProgressNode;
 import com.roadmaker.roadmap.entity.roadmap.Roadmap;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter
@@ -12,7 +14,14 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class RoadmapResponse {
-    private RoadmapDto roadmap;
+    private Long id;
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+    private MemberResponse member;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
     private RoadmapViewportDto viewport;
     private List<RoadmapEdgeDto> edges;
     private List<RoadmapNodeDto> nodes;
@@ -20,7 +29,13 @@ public class RoadmapResponse {
     /** 로그인 하지 않거나, 로드맵에 참가하지 않은 사람의 DTO */
     public static RoadmapResponse of(Roadmap roadmap) {
         return RoadmapResponse.builder()
-                .roadmap(RoadmapDto.of(roadmap, roadmap.getMember()))
+                .id(roadmap.getId())
+                .title(roadmap.getTitle())
+                .description(roadmap.getDescription())
+                .thumbnailUrl(roadmap.getThumbnailUrl())
+                .member(MemberResponse.of(roadmap.getMember()))
+                .createdAt(roadmap.getCreatedAt())
+                .updatedAt(roadmap.getUpdatedAt())
                 .viewport(RoadmapViewportDto.of(roadmap.getRoadmapViewport()))
                 .nodes(roadmap.getRoadmapNodes().stream().map(RoadmapNodeDto::of).toList())
                 .edges(roadmap.getRoadmapEdges().stream().map(RoadmapEdgeDto::of).toList())
@@ -30,8 +45,13 @@ public class RoadmapResponse {
     /** 로드맵에 참가한 사람의 DTO */
     public static RoadmapResponse of(Roadmap roadmap, List<InProgressNode> inProgressNodes) {
         return RoadmapResponse.builder()
-                .roadmap(RoadmapDto.of(roadmap, roadmap.getMember()))
-                .viewport(RoadmapViewportDto.of(roadmap.getRoadmapViewport()))
+                .id(roadmap.getId())
+                .title(roadmap.getTitle())
+                .description(roadmap.getDescription())
+                .thumbnailUrl(roadmap.getThumbnailUrl())
+                .member(MemberResponse.of(roadmap.getMember()))
+                .createdAt(roadmap.getCreatedAt())
+                .updatedAt(roadmap.getUpdatedAt())                .viewport(RoadmapViewportDto.of(roadmap.getRoadmapViewport()))
                 .nodes(inProgressNodes.stream().map(RoadmapNodeDto::of).toList())
                 .edges(roadmap.getRoadmapEdges().stream().map(RoadmapEdgeDto::of).toList())
                 .build();
