eaf9872610262b1d2145a5ff8c27a2bdc4e6ec79
Heeho Lim <heeho1998@gmail.com>
2023-08-02 17:15:38 +0900

    [#157] Refactor : change response entity to boolean

--- src/main/java/com/roadmaker/like/controller/LikeController.java
+++ src/main/java/com/roadmaker/like/controller/LikeController.java
@@ -24,20 +24,19 @@ public class LikeController {
     private final LikeService likeService;
     @LoginRequired
     @PostMapping("/like-roadmap/{roadmapId}")
-    public ResponseEntity<Integer> likeRoadmap(@PathVariable Long roadmapId, @LoginMember Member member) {
+    public ResponseEntity<Boolean> likeRoadmap(@PathVariable Long roadmapId, @LoginMember Member member) {
         if (likeService.isLiked(roadmapId, member.getId())) {
-            // Unlike the roadmap
+            // 이미 좋아요 상태면 좋아요 취소
             likeService.unlikeRoadmap(roadmapId, member.getId());
+            return ResponseEntity.ok(false);
         } else {
-            // Like the roadmap
+            // 로드맵 좋아요
             likeService.likeRoadmap(roadmapId, member.getId());
+            return ResponseEntity.ok(true);
         }
-
-        // Get the updated like count
-        int likeCount = likeService.getLikeCount(roadmapId);
-        return ResponseEntity.ok(likeCount);
     }
 
+    // 좋아요 갯수를 알려주는 API
     @GetMapping("/roadmap/{roadmapId}/likeCount")
     public ResponseEntity<Integer> getLikeCount(@PathVariable Long roadmapId) {
         int likeCount = likeService.getLikeCount(roadmapId);
