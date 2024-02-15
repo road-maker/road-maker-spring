c038d771827b438c6281160acd798a54962e4e52
Wonyoung Kang <onezerokang@gmail.com>
2024-02-15 10:53:53 +0900

    [#268] Fix: EmailAlreadyRegisteredException의 httpStatus를 CONFLICT로 수정

--- src/main/java/com/roadmaker/member/exception/EmailAlreadyRegisteredException.java
+++ src/main/java/com/roadmaker/member/exception/EmailAlreadyRegisteredException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class EmailAlreadyRegisteredException extends ApiException {
     public EmailAlreadyRegisteredException() {
-        super(HttpStatus.BAD_REQUEST, "이미 등록된 이메일입니다.");
+        super(HttpStatus.CONFLICT, "이미 등록된 이메일입니다.");
     }
 }
