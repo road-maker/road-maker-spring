b0c882dc46d3930b31c85e53e23bfd525c49cd45
Wonyoung Kang <onezerokang@gmail.com>
2024-02-15 16:49:49 +0900

    [#275] Refactor: 존재하지 않는 로드맵에 댓글 생성 시 예외 던지기

--- src/main/java/com/roadmaker/comment/service/CommentService.java
+++ src/main/java/com/roadmaker/comment/service/CommentService.java
@@ -5,7 +5,9 @@ import com.roadmaker.comment.dto.CommentResponse;
 import com.roadmaker.member.entity.Member;
 
 public interface CommentService {
-    public CommentResponse findCommentByRoadmapIdAndPageRequest (Long roadmapId, Integer page, Integer size);
-    public CommentResponse findByMemberIdAndPageRequest (Long memberId, Integer page, Integer size);
-    public boolean saveComment (CommentDto commentDto, Member member);
-    }
+    public CommentResponse findCommentByRoadmapIdAndPageRequest(Long roadmapId, Integer page, Integer size);
+
+    public CommentResponse findByMemberIdAndPageRequest(Long memberId, Integer page, Integer size);
+
+    public void saveComment(CommentDto commentDto, Member member);
+}
