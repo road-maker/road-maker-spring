9601d2ab6f54a444b493659258efe31f854f257c
Wonyoung Kang <onezerokang@gmail.com>
2024-03-13 23:25:04 +0900

    [##283] Refactor: ImageService interface 제거

--- src/main/java/com/roadmaker/image/service/ImageService.java
+++ src/main/java/com/roadmaker/image/service/ImageService.java
@@ -1,9 +1,35 @@
 package com.roadmaker.image.service;
 
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
-public interface ImageService {
-    public String uploadImage(MultipartFile image) throws IOException;
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final S3Template s3Template;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Transactional
+    public String uploadImage(MultipartFile image) throws IOException {
+        String originalFilename = image.getOriginalFilename();
+        String extension = StringUtils.getFilenameExtension(originalFilename);
+
+        String uuidImageName = UUID.randomUUID().toString() + "." + extension;
+
+        S3Resource s3Resource = s3Template.upload(bucketName, uuidImageName, image.getInputStream(), ObjectMetadata.builder().contentType(extension).build());
+
+        return s3Resource.getURL().toString();
+    }
 }
