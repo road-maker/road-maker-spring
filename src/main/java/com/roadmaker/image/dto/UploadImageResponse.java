0f6dea3468374307d98a690c161569ee5c0064e7
Wonyoung Kang <onezerokang@gmail.com>
2024-03-16 23:24:28 +0900

    Refactor: UploadImageResponse 불필요한 lombok 제거

--- src/main/java/com/roadmaker/image/dto/UploadImageResponse.java
+++ src/main/java/com/roadmaker/image/dto/UploadImageResponse.java
@@ -4,9 +4,11 @@ package com.roadmaker.image.dto;
 import lombok.*;
 
 @Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
 public class UploadImageResponse {
-    private String url;
+    private final String url;
+
+    @Builder
+    private UploadImageResponse(String url) {
+        this.url = url;
+    }
 }
