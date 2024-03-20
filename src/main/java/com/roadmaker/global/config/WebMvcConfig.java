2ee240b0620340bcb6fcef73adb97861b2c10575
Wonyoung Kang <onezerokang@gmail.com>
2024-03-20 17:15:29 +0900

    Fix: CORS 설절 복구

--- src/main/java/com/roadmaker/global/config/WebMvcConfig.java
+++ src/main/java/com/roadmaker/global/config/WebMvcConfig.java
@@ -5,10 +5,13 @@ import com.roadmaker.global.resolver.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,5 +28,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(
+                        GET.name(),
+                        POST.name(),
+                        PUT.name(),
+                        PATCH.name(),
+                        DELETE.name())
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
