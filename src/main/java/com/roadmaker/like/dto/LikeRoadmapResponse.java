390582ee28db7a3a90fa3ef356746b27d1bcbc96
Wonyoung Kang <onezerokang@gmail.com>
2024-03-20 16:13:10 +0900

    Fix: record 클래스의 @Getter, @RequiredArgsConstructor lombok 제거

--- src/main/java/com/roadmaker/like/dto/LikeRoadmapResponse.java
+++ src/main/java/com/roadmaker/like/dto/LikeRoadmapResponse.java
@@ -2,10 +2,7 @@ package com.roadmaker.like.dto;
 
 import lombok.*;
 
-@Getter
-@ToString
 @Builder
-@RequiredArgsConstructor
 public record LikeRoadmapResponse(Boolean isLiked, Integer likeCount) {
     public static LikeRoadmapResponse from(boolean isLiked, int likeCount) {
         return LikeRoadmapResponse.builder()
--- src/main/java/com/roadmaker/member/dto/response/MemberLoginResponse.java
+++ src/main/java/com/roadmaker/member/dto/response/MemberLoginResponse.java
@@ -3,10 +3,7 @@ package com.roadmaker.member.dto.response;
 import com.roadmaker.member.entity.Member;
 import lombok.*;
 
-@Getter
-@ToString
 @Builder
-@RequiredArgsConstructor
 public record MemberLoginResponse(TokenInfo tokenInfo, MemberResponse member) {
     public static MemberLoginResponse of(Member member, TokenInfo tokenInfo) {
         return MemberLoginResponse.builder()
